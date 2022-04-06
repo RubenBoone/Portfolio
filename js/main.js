87f5c1ba1a3a8026062c4737344d84fe38ee22e4
Ruben Boone <r0785519@student.thomasmore.be>
2022-04-06 09:53:08 +0200

    init

---
+++ js/main.js
@@ -0,0 +1,43 @@
+// Nice scroll border
+window.onscroll = function () {
+  scrollFunction();
+};
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById('navbar').style.background = '#080a10';
+    document.getElementById('mobNav').style.background = '#080a10';
+  } else {
+    if (!open) {
+      document.getElementById('navbar').style.background = 'none';
+      document.getElementById('mobNav').style.background = 'none';
+    }
+  }
+}
+
+// Mobile navigation
+let open;
+$('#toggleMobNav, .mobLink').click(function name() {
+  if (open) {
+    $('.mobLink').toggleClass('hide');
+    if (
+      document.body.scrollTop > 20 ||
+      document.documentElement.scrollTop > 20
+    ) {
+    } else {
+      document.getElementById('mobNav').style.background = 'none';
+    }
+    open = false;
+  } else {
+    $('.mobLink').toggleClass('hide');
+    document.getElementById('mobNav').style.background = '#080a10';
+    open = true;
+  }
+});
+
+$(window).resize(function () {
+  if ($(window).width() > 600) {
+    $('#mobLinks').addClass('hide');
+  } else {
+    $('#mobLinks').removeClass('hide');
+  }
+});
