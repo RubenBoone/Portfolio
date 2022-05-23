0ea68c501d947851df2400e96854030d296a8b11
Ruben Boone <r0785519@student.thomasmore.be>
2022-05-23 13:00:05 +0200

    language change v2

--- js/main.js
+++ js/main.js
@@ -1,44 +1,44 @@
 // Nice scroll border
 window.onscroll = function () {
-  scrollFunction();
+	scrollFunction();
 };
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById('navbar').style.background = '#080a10';
-    document.getElementById('mobNav').style.background = '#080a10';
-    $('#languageTip').addClass('fadeOut');
-  } else {
-    if (!open) {
-      document.getElementById('navbar').style.background = 'none';
-      document.getElementById('mobNav').style.background = 'none';
-    }
-  }
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+		document.getElementById('navbar').style.background = '#080a10';
+		document.getElementById('mobNav').style.background = '#080a10';
+		$('#languageTip').addClass('fadeOut');
+	} else {
+		if (!open) {
+			document.getElementById('navbar').style.background = 'none';
+			document.getElementById('mobNav').style.background = 'none';
+		}
+	}
 }
 
 // Mobile navigation
 let open;
 $('#toggleMobNav, .mobLink').click(function name() {
-  if (open) {
-    $('.mobLink').toggleClass('hide');
-    if (
-      document.body.scrollTop > 20 ||
-      document.documentElement.scrollTop > 20
-    ) {
-    } else {
-      document.getElementById('mobNav').style.background = 'none';
-    }
-    open = false;
-  } else {
-    $('.mobLink').toggleClass('hide');
-    document.getElementById('mobNav').style.background = '#080a10';
-    open = true;
-  }
+	if (open) {
+		$('.mobLink').toggleClass('hide');
+		if (
+			document.body.scrollTop > 20 ||
+			document.documentElement.scrollTop > 20
+		) {
+		} else {
+			document.getElementById('mobNav').style.background = 'none';
+		}
+		open = false;
+	} else {
+		$('.mobLink').toggleClass('hide');
+		document.getElementById('mobNav').style.background = '#080a10';
+		open = true;
+	}
 });
 
 $(window).resize(function () {
-  if ($(window).width() > 1000) {
-    $('#mobLinks').addClass('hide');
-  } else {
-    $('#mobLinks').removeClass('hide');
-  }
+	if ($(window).width() > 1000) {
+		$('#mobLinks').addClass('hide');
+	} else {
+		$('#mobLinks').removeClass('hide');
+	}
 });
